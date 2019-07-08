8c396df393f9e0fbcde56a1fc8a5fc14f7ef4c05
everydayduffy <james.philip.duffy@gmail.com>
2019-07-08 15:02:11 +0100

    added more invls tests

--- tests/testthat/test-invls.R
+++ tests/testthat/test-invls.R
@@ -7,4 +7,6 @@ ls1 <- invls(dtm100m, raster::extent(dtm1m), 180)
 
 test_that("invls output", {
   expect_is(ls1, "RasterLayer")
+  expect_equal(raster::res(dtm100m), raster::res(ls1))
+  expect_equal(raster::extent(dtm1m), raster::extent(ls1))
 })
